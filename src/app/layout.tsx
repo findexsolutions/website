cb96c246053aae2172ae4c7fb1701b2ea99ff80b
Luiz Galvão <luizmedeiros327@gmail.com>
2026-02-25 15:26:15 -0300

    starting the project and add the visual identity

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Exo_2 } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const exo2 = Exo_2({
+  variable: "--font-exo2",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Findex Solutions",
+  description: "Findex Solutions is a software development company.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${exo2.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-fsgold">Findex Solutions</h1>
+    </main>
+  );
+}
